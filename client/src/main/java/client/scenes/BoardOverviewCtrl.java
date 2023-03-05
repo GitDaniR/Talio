1f87ad32c3b6c5ea710500a965b6705d3145d06f
Alexandru Fazakas <A.Fazakas@student.tudelft.nl>
2023-03-05 12:58:39 +0100

    Added controllers for board and list

---
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -0,0 +1,57 @@
+package client.scenes;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import com.google.inject.Inject;
+
+import client.utils.ServerUtils;
+import commons.BoardList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import org.checkerframework.checker.units.qual.C;
+
+public class BoardOverviewCtrl implements Initializable {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    private ObservableList<BoardList> data;
+
+    @FXML
+    private FlowPane mainBoard;
+
+    @Inject
+    public BoardOverviewCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void addList() {
+        mainCtrl.showAddList();
+        refresh();
+    }
+
+    public void refresh() {
+        //var lists = server.getLists(); This method will be in antono's code
+        var lists = new ArrayList<BoardList>();
+        lists.add(new BoardList("Blabla",null));
+        data = FXCollections.observableList(lists);
+        for(int i=0;i<data.size();i++){
+            CustomListCtrl listObject = new CustomListCtrl();
+            //listObject.getChildren().add(new Label(data.get(i).title));
+            mainBoard.getChildren().add(listObject);
+        }
+    }
+}
