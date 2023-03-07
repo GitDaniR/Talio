ca4b0465cf37b41c23e6a13ef6e6c9bca3c9006e
Una Jaćimović <u.jacimovic-1@student.tudelft.nl>
2023-03-07 22:50:43 +0100

    Fake server utils

---
+++ client/src/main/java/client/utils/FakeServerUtils.java
@@ -0,0 +1,77 @@
+package client.utils;
+
+import commons.Board;
+import commons.BoardList;
+import commons.Card;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.GenericType;
+import org.checkerframework.checker.units.qual.C;
+import org.glassfish.jersey.client.ClientConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+
+public class FakeServerUtils {
+
+    public List<BoardList> getBoardLists() {
+        List<BoardList> lists = new ArrayList<>();
+
+        BoardList list1 = new BoardList("TODO");
+        BoardList list2 = new BoardList("TASKS");
+        BoardList list3  = new BoardList("THINGS");
+
+        lists.add(list1);
+        lists.add(list2);
+        lists.add(list3);
+
+        return lists;
+
+
+
+    }
+
+    public List<Card> getCards(BoardList){
+
+        List<Card> cards = new ArrayList<>();
+
+        Card card1 = new Card("Card1", "Description", 0 , null);
+        Card card2 = new Card("Card2", "Description", 0, null);
+        Card card3 = new Card("Card3", "Description", 0 , null);
+
+        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+
+        return cards;
+
+
+    }
+    public List<Card> getCards(int id){
+
+        List<Card> cards = new ArrayList<>();
+
+        Card card1 = new Card("Card1", "Description", 0 , null);
+        Card card2 = new Card("Card2", "Description", 0, null);
+        Card card3 = new Card("Card3", "Description", 0 , null);
+
+        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+
+        return cards;
+
+    }
+    public void addBoardList(BoardList list) {
+        System.out.println("Successfully added the list");
+        return;
+
+    }
+
+    public void addCard(Card card){
+        System.out.println("Successfully added the card");
+        return;
+    }
+}
