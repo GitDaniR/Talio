a9c848c5aed55bd5f1b31653f0f77120f4f389a9
Rūta Giedrytė <r.giedryte@student.tudelft.nl>
2023-03-07 07:17:35 +0100

    Building structure for the card scenes

--- client/src/main/java/client/scenes/CardCtrl.java
+++ client/src/main/java/client/scenes/CardCtrl.java
@@ -1,2 +1,20 @@
-package client.scenes;public class CardCtrl {
+package client.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardCtrl implements Initializable{
+    @FXML
+    private Label CardTitle;
+    @FXML
+    private Button delete;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
 }
