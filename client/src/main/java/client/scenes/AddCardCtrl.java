fbf063e3d34a470ff06c73e6c57d0d837aa534ca
Rūta Giedrytė <r.giedryte@student.tudelft.nl>
2023-03-08 09:29:26 +0100

    Fixing the unused imports

--- client/src/main/java/client/scenes/AddCardCtrl.java
+++ client/src/main/java/client/scenes/AddCardCtrl.java
@@ -11,13 +11,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import static javafx.scene.input.KeyCode.ENTER;
 
 public class AddCardCtrl implements Initializable {
 
