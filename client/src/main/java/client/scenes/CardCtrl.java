4565ae5fb8097d7c021078ccc3de998bb6b7e277
Alexandru Fazakas <A.Fazakas@student.tudelft.nl>
2023-03-09 20:50:20 +0100

    Deleted constructor from cardCtrl as it was not injecting properly

--- client/src/main/java/client/scenes/CardCtrl.java
+++ client/src/main/java/client/scenes/CardCtrl.java
@@ -1,14 +1,11 @@
 package client.scenes;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,16 +17,6 @@ public class CardCtrl extends AnchorPane implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
-
-    public CardCtrl(){
-        super();
-        try
-        {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Card.fxml"));
-            Node n = loader.load();
-            this.getChildren().add(n);
-        } catch (IOException ix){}
-    }
 
     /** Sets text of the title of the card
      * @param text
