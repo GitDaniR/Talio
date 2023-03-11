f8c2468a3881bfab4e25ae625b0bcc9b6542b40b
Daniel Rachev <d.n.rachev@student.tudelft.nl>
2023-03-11 21:29:13 +0100

    Generated test files

---
+++ server/src/test/java/server/services/BoardServiceTest.java
@@ -0,0 +1,29 @@
+package server.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
