b4215d9a5aa559f0847744fa239044a804bc7ca4
Daniel Rachev <d.n.rachev@student.tudelft.nl>
2023-03-12 13:41:09 +0100

    Added test for getById on success

--- server/src/test/java/server/services/BoardServiceTest.java
+++ server/src/test/java/server/services/BoardServiceTest.java
@@ -23,6 +23,7 @@ class BoardServiceTest {
         b1 = new Board("First", "123");
         b2 = new Board("Second", "456");
         b3 = new Board("Third", "789");
+        this.boards = new ArrayList<>();
         boards.add(b1);
         boards.add(b2);
         boards.add(b3);
@@ -49,7 +50,13 @@ class BoardServiceTest {
     }
 
     @Test
-    void getById() {
+    public void testGetByIdSuccess() throws Exception {
+        Board result = sut.getById(2);
+        Board expected = b2;
+        List<String> expectedCalls = new ArrayList<>();
+        expectedCalls.add(TestBoardRepository.FIND_BY_ID);
+        assertEquals(expected, result);
+        assertEquals(expectedCalls, repo.getCalls());
     }
 
     @Test
