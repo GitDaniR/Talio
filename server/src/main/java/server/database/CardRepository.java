8993584df38f5b6882bc25f11d96b8060dc6b4af
Rūta Giedrytė <r.giedryte@student.tudelft.nl>
2023-03-15 17:31:27 +0100

    Deleting unused imports (again)

--- server/src/main/java/server/database/CardRepository.java
+++ server/src/main/java/server/database/CardRepository.java
@@ -21,7 +21,6 @@ import commons.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CardRepository extends JpaRepository<Card, Integer> {
     @Modifying
