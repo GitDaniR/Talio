16ad6891f91238e20fed75706baf228f4c22d0b7
Una Jaćimović <u.jacimovic-1@student.tudelft.nl>
2023-03-15 20:21:38 +0100

    Creating dummy hbox objects for showing joined boards

---
+++ client/src/main/java/client/scenes/BoardDummy.java
@@ -0,0 +1,43 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import commons.Board;
+import commons.BoardList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoardDummy implements Initializable {
+
+    @FXML
+    private TextField boardTitle;
+    @FXML
+    private Button openButton;
+    @FXML
+    private Button leaveButton;
+
+    private Board board;
+
+    private ServerUtils server;
+    private MainCtrl mainCtrl;
+
+    public void setServerAndCtrl(ServerUtils server,MainCtrl mainCtrl){
+        this.server=server;
+        this.mainCtrl=mainCtrl;
+    }
+
+    public void setBoard(Board board){
+        this.board = board;
+        this.boardTitle.setText(board.title);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
