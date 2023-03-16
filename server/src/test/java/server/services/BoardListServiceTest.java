cea4f4eee048f7979e0e196d59d6cbcc500215b5
Daniel Rachev <d.n.rachev@student.tudelft.nl>
2023-03-16 22:40:00 +0100

    Added BoardListServiceTest file

---
+++ server/src/test/java/server/services/BoardListServiceTest.java
@@ -0,0 +1,29 @@
+package server.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardListServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void updateTitleById() {
+    }
+}
