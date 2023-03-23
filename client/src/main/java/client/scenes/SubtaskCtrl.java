d26369654436ea27e803881076f3d5d1543fa95b
Una Jaćimović <u.jacimovic-1@student.tudelft.nl>
2023-03-23 11:52:44 +0100

    Added custom cell for listview and implemented controller for every subtask

---
+++ client/src/main/java/client/scenes/SubtaskCtrl.java
@@ -0,0 +1,51 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import commons.Subtask;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SubtaskCtrl implements Initializable {
+
+    private  ServerUtils server;
+    private  MainCtrl mainCtrl;
+
+    @FXML
+    TextField title;
+
+    @FXML
+    Button check;
+
+    @FXML
+    Button remove;
+
+    private Subtask subtask;
+    public void setServerAndCtrl(ServerUtils server, MainCtrl mainCtrl){
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
+    public void finishTask(){
+        server.updateSubtaskStatus(subtask.id, true);
+    }
+
+    public void unfinishTask(){
+        server.updateSubtaskStatus(subtask.id, false);
+    }
+
+    public void removeTask(){
+        server.removeSubtask(subtask.id);
+    }
+    public void setSubtask(Subtask subtask){
+        this.subtask = subtask;
+
+    }
+}
