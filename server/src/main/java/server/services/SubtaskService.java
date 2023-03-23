f1346ad2dfd879470bde5464b64193a81d4ba16f
Antonio <a.f.lupu@student.tudelft.nl>
2023-03-23 12:18:28 +0100

    Added SubtaskService in server folder

--- server/src/main/java/server/services/SubtaskService.java
+++ server/src/main/java/server/services/SubtaskService.java
@@ -1,2 +1,65 @@
-package server.services;public class SubtaskService {
+package server.services;
+
+import commons.Subtask;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import server.database.SubtaskRepository;
+
+@Service
+public class SubtaskService {
+
+    private final SubtaskRepository repo;
+
+    public SubtaskService(SubtaskRepository repo) {
+        this.repo = repo;
+    }
+
+
+    /**
+     * Method which returns a subtask by id from repo.
+     * @param id
+     * @return user
+     * @throws Exception if id is not in repo.
+     */
+    public ResponseEntity<Subtask> getById(Integer id) throws Exception {
+        if (id < 0 || !repo.existsById(id)) {
+            throw new Exception("Invalid id");
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
+
+
+    /**
+     * Method which adds a new subtask to repo.
+     * @param subtask
+     * @return the saved board
+     * @throws Exception if title is null.
+     */
+    public ResponseEntity<Subtask> add(Subtask subtask) throws Exception {
+        if (subtask.title == null) {
+            throw new Exception("Invalid title");
+        }
+        return ResponseEntity.ok(repo.save(subtask));
+    }
+
+
+    /**
+     * Method which deletes a subtask by id from repo.
+     * @param id
+     * @return the deleted subtask
+     * @throws Exception if id is not in repo.
+     */
+    public ResponseEntity<Subtask> deleteById(Integer id) throws Exception {
+        if (id < 0 || !repo.existsById(id)) {
+            throw new Exception("Invalid id");
+        }
+        ResponseEntity<Subtask> deletedRecord = ResponseEntity.ok(repo.findById(id).get());
+        repo.deleteById(id);
+        return deletedRecord;
+    }
+
+    public SubtaskRepository getRepo(){
+        return this.repo;
+    }
+
 }
