c3fb12508d365a1f58c2b06e0d0ed04a6b8d24af
Rūta Giedrytė <r.giedryte@student.tudelft.nl>
2023-03-25 17:00:10 +0100

    Made tag scene and controller to show up in overview

---
+++ client/src/main/java/client/scenes/TagDisplayCtrl.java
@@ -0,0 +1,55 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import commons.Tag;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TagDisplayCtrl implements Initializable {
+
+    @FXML
+    private Label lblTagName;
+
+    @FXML
+    private HBox box;
+
+    private MainCtrl mainCtrl;
+    private ServerUtils server;
+
+    private TagOverviewCtrl tagOverviewCtrl;
+    private Tag tag;
+
+    public void setMainCtrlAndServer(MainCtrl mainCtrl, ServerUtils server,
+                                     TagOverviewCtrl tagOverviewCtrl){
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+        this.tagOverviewCtrl = tagOverviewCtrl;
+    }
+
+    public void setTag(Tag tagToBeSet){
+        this.tag = tagToBeSet;
+        lblTagName.setText(tag.title);
+        box.setStyle("-fx-background-color: " + tag.color.replace("0x", "#") + ";-fx-border-color: black;-fx-border-width: 2px;");
+    }
+
+    public void editTag(){
+        mainCtrl.showEditTag(tag);
+    }
+
+    public void deleteTag(){
+        server.deleteTag(tag.id);
+        this.tagOverviewCtrl.refresh();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
