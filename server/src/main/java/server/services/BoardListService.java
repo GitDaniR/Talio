6f63f3a321f5636f2a103e8cff15ea975ddb06bd
Alexandru Fazakas <A.Fazakas@student.tudelft.nl>
2023-03-25 13:47:11 +0100

    Deleted unused import

--- server/src/main/java/server/services/BoardListService.java
+++ server/src/main/java/server/services/BoardListService.java
@@ -1,7 +1,6 @@
 package server.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
